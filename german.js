3b1100881d8ee05dcf35bf376992b47b1c2d01ee
Kateryna <katerinashkrob62@gmail.com>
2023-06-05 00:49:18 +0300

    Made questions, button and add new pictures

--- german.js
+++ german.js
@@ -3,7 +3,15 @@ document.getElementById("text").innerHTML = test;
 
 var step=0
 
+var pics = ["cat.jpg", "dog.jpg", "hamster.jpg"] 
+
+var text = ["Ist das eine Katze?", "Ist das ein Schaf?", "Ist das ein Kanninchen?"]
+document.getElementById("question").innerHTML = text[step];
+
 function nextStep(){
-    console.log("bla")
     step = step + 1;
-}
+        if (step < pics.length) {
+            document.getElementById("picture").src= pics [step];
+            document.getElementById("question").innerHTML = text[step];
+        }
+    }
